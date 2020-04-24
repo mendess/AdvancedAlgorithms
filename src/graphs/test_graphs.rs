0deaa7c72eea85874fdee886fb4b906c8614a5b0
mendess <pedro.mendes.26@gmail.com>
2020-04-24 16:34:35 +0100

    Preallocate HashSet

--- src/graphs/test_graphs.rs
+++ src/graphs/test_graphs.rs
@@ -1,5 +1,5 @@
 use super::{Graph, Vertex};
-use crate::{util::small_hash_map::{SmallSet, Set}, graph};
+use crate::graph;
 use rand::{
     distributions::{uniform::SampleUniform, Distribution, Uniform},
     Rng,
@@ -11,128 +11,93 @@ use std::{
     ops::{Div, Mul, Sub},
 };
 
-pub fn graph_one<G: Graph<NodeId = usize>>() -> G {
+pub fn graph_one<G: Graph<NodeId = N>, N: Vertex>() -> G {
     graph!(G = (10) {
-        0 => 1;
-        0 => 2;
-        0 => 3;
-        0 => 4;
-        1 => 0;
-        1 => 2;
-        1 => 3;
-        1 => 4;
-        2 => 0;
-        2 => 1;
-        2 => 3;
-        2 => 4;
-        3 => 0;
-        3 => 1;
-        3 => 2;
-        3 => 4;
-        4 => 0;
-        4 => 1;
-        4 => 2;
-        4 => 3;
+       N::from(0) => N::from(1);
+       N::from(0) => N::from(2);
+       N::from(0) => N::from(3);
+       N::from(0) => N::from(4);
+       N::from(1) => N::from(0);
+       N::from(1) => N::from(2);
+       N::from(1) => N::from(3);
+       N::from(1) => N::from(4);
+       N::from(2) => N::from(0);
+       N::from(2) => N::from(1);
+       N::from(2) => N::from(3);
+       N::from(2) => N::from(4);
+       N::from(3) => N::from(0);
+       N::from(3) => N::from(1);
+       N::from(3) => N::from(2);
+       N::from(3) => N::from(4);
+       N::from(4) => N::from(0);
+       N::from(4) => N::from(1);
+       N::from(4) => N::from(2);
+       N::from(4) => N::from(3);
 
-        5 => 6;
-        5 => 7;
-        5 => 8;
-        5 => 9;
-        6 => 5;
-        6 => 7;
-        6 => 8;
-        6 => 9;
-        7 => 5;
-        7 => 6;
-        7 => 8;
-        7 => 9;
-        8 => 5;
-        8 => 6;
-        8 => 7;
-        8 => 9;
-        9 => 5;
-        9 => 6;
-        9 => 7;
-        9 => 8;
-        // the min cut
-        2 => 6;
-        4 => 5;
-        3 => 7;
+       N::from(5) => N::from(6);
+       N::from(5) => N::from(7);
+       N::from(5) => N::from(8);
+       N::from(5) => N::from(9);
+       N::from(6) => N::from(5);
+       N::from(6) => N::from(7);
+       N::from(6) => N::from(8);
+       N::from(6) => N::from(9);
+       N::from(7) => N::from(5);
+       N::from(7) => N::from(6);
+       N::from(7) => N::from(8);
+       N::from(7) => N::from(9);
+       N::from(8) => N::from(5);
+       N::from(8) => N::from(6);
+       N::from(8) => N::from(7);
+       N::from(8) => N::from(9);
+       N::from(9) => N::from(5);
+       N::from(9) => N::from(6);
+       N::from(9) => N::from(7);
+       N::from(9) => N::from(8);
+       // the min cut
+       N::from(2) => N::from(6);
+       N::from(4) => N::from(5);
+       N::from(3) => N::from(7);
     })
 }
 
-pub fn random_graph<G, N, R>(n: N, m: usize, rng: R) -> G
+pub fn random_graph<G, N, R>(n: N, m: usize, mut rng: R) -> G
 where
     G: Graph<NodeId = N>,
     N: Vertex + SampleUniform,
     R: Rng,
 {
-    let edges = Vec::with_capacity(m);
+    let mut edges = Vec::with_capacity(m);
     let dist = Uniform::from(N::from(0)..n);
-    fn run<H, Hs, G, N, R>(
-        n: N,
-        mut edges: Vec<(N, N)>,
-        dist: Uniform<N>,
-        mut rng: R,
-        mut set: H,
-    ) -> G
-    where
-        N: Vertex + SampleUniform,
-        H: Set<(N, N), Hs>,
-        G: Graph<NodeId = N>,
-        R: Rng,
-    {
-        while edges.len() < edges.capacity() {
-            let a0 = dist.sample(&mut rng);
-            let a1 = dist.sample(&mut rng);
-            if a0 != a1 && set.insert((a0, a1)) {
-                edges.push((a0, a1))
-            }
+    let mut set = HashSet::with_capacity_and_hasher(m, Default::default());
+    while edges.len() < edges.capacity() {
+        let a0 = dist.sample(&mut rng);
+        let a1 = dist.sample(&mut rng);
+        if a0 != a1 && set.insert((a0, a1)) {
+            edges.push((a0, a1))
         }
+    }
 
-        G::new(n.into(), edges.into_iter())
-    }
-    if m < 50 {
-        run(n, edges, dist, rng, SmallSet::with_capacity(m))
-    } else {
-        run(n, edges, dist, rng, HashSet::with_capacity_and_hasher(m, Default::default()))
-    }
+    G::new(n.into(), edges.into_iter())
 }
 
-pub fn random_graph_concrete<G, R>(n: usize, m: usize, rng: R) -> G
+pub fn random_graph_concrete<G, R>(n: usize, m: usize, mut rng: R) -> G
 where
     G: Graph<NodeId = usize>,
     R: Rng,
 {
-    let edges = Vec::with_capacity(m);
+    let mut edges = Vec::with_capacity(m);
     let dist = Uniform::from(0..n);
-    fn run<H, Hs, G, R>(
-        n: usize,
-        mut edges: Vec<(usize, usize)>,
-        dist: Uniform<usize>,
-        mut rng: R,
-        mut set: H,
-    ) -> G
-    where
-        H: Set<(usize, usize), Hs>,
-        G: Graph<NodeId = usize>,
-        R: Rng,
-    {
-        while edges.len() < edges.capacity() {
-            let a0 = dist.sample(&mut rng);
-            let a1 = dist.sample(&mut rng);
-            if a0 != a1 && set.insert((a0, a1)) {
-                edges.push((a0, a1))
-            }
+    let mut set = HashSet::with_capacity_and_hasher(m, Default::default());
+    while edges.len() < edges.capacity() {
+        let a0 = dist.sample(&mut rng);
+        let a1 = dist.sample(&mut rng);
+        if a0 != a1 && set.insert((a0, a1)) {
+            edges.push((a0, a1))
         }
+    }
 
-        G::new(n, edges.into_iter())
-    }
-    if m < 50 {
-        run(n, edges, dist, rng, SmallSet::with_capacity(m))
-    } else {
-        run(n, edges, dist, rng, HashSet::with_capacity_and_hasher(m, Default::default()))
-    }
+    G::new(n, edges.into_iter())
 }
 
 /// Generates a graph using the Evdos Ronmi method.
