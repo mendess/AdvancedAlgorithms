91e4da67fdf4a72f9c37d1487bcf35878e0e57a0
mendess <pedro.mendes.26@gmail.com>
2020-04-24 11:40:52 +0100

    Improve benchmark

--- benches/random_graphs.rs
+++ benches/random_graphs.rs
@@ -3,7 +3,6 @@ use aava::graphs::{
     test_graphs::{random_graph_er, random_graph_er_concrete},
 };
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use itertools::Itertools;
 use rand::{rngs::SmallRng, SeedableRng};
 
 pub fn random_graph_er_bench(c: &mut Criterion) {
@@ -13,21 +12,25 @@ pub fn random_graph_er_bench(c: &mut Criterion) {
         .zip(&[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7])
         .map(|(n, &p)| (n, p, (n as f64) * p))
         .map(|(n, p, e)| (n, p, e.floor() as usize))
-        .sorted_by_key(|e| e.2)
-        .dedup_by(|a, b| a.2 == b.2)
         .collect::<Vec<_>>();
     for (n, p, e) in params {
         group.throughput(Throughput::Elements(e as u64));
-        group.bench_with_input(BenchmarkId::new("generic", e), &(n, p), |b, &i| {
-            b.iter(|| {
-                random_graph_er::<EdgeList, usize, _>(
-                    i.0,
-                    i.1,
-                    black_box(SmallRng::seed_from_u64(0x0DDB1A5E5BAD5EEDu64)),
-                )
-            })
-        });
-        group.bench_with_input(BenchmarkId::new("concrete", e), &(n, p), |b, &i| {
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("generic_{}_{}_{}", n, p, e)),
+            &(n, p),
+            |b, &i| {
+                b.iter(|| {
+                    random_graph_er::<EdgeList, usize, _>(
+                        i.0,
+                        i.1,
+                        black_box(SmallRng::seed_from_u64(0x0DDB1A5E5BAD5EEDu64)),
+                    )
+                })
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("concrete_{}_{}_{}", n, p, e)),
+            &(n, p), |b, &i| {
             b.iter(|| {
                 random_graph_er_concrete::<EdgeList, _>(
                     i.0,
