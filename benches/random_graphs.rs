d69e4c07730317fdd2264809ebc8641c12e41b6c
mendess <pedro.mendes.26@gmail.com>
2020-04-24 12:29:18 +0100

    ...

--- benches/random_graphs.rs
+++ benches/random_graphs.rs
@@ -7,7 +7,7 @@ use rand::{rngs::SmallRng, SeedableRng};
 
 pub fn random_graph_er_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("RandomGraphER");
-    let params = (1..=9)
+    let params = (1_usize..=9)
         .map(|i| i * 100)
         .flat_map(|n| [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7].iter().map(move |&p| (n, p)))
         .map(|(n, p)| (n, p, (n as f64) * p))
@@ -15,8 +15,7 @@ pub fn random_graph_er_bench(c: &mut Criterion) {
         .collect::<Vec<_>>();
     for (n, p, e) in params {
         group.throughput(Throughput::Elements(e as u64));
-        group.bench_with_input(
-            BenchmarkId::from_parameter(format!("generic_{}_{}_{}", n, p, e)),
+        group.bench_with_input(BenchmarkId::new("generic", format!("{}_{}_{}", n, p, e)),
             &(n, p),
             |b, &i| {
                 b.iter(|| {
@@ -28,17 +27,16 @@ pub fn random_graph_er_bench(c: &mut Criterion) {
                 })
             },
         );
-        group.bench_with_input(
-            BenchmarkId::from_parameter(format!("concrete_{}_{}_{}", n, p, e)),
+        group.bench_with_input(BenchmarkId::new("concrete", format!("{}_{}_{}", n, p, e)),
             &(n, p), |b, &i| {
-            b.iter(|| {
-                random_graph_er_concrete::<EdgeList, _>(
-                    i.0,
-                    i.1,
-                    black_box(SmallRng::seed_from_u64(0x0DDB1A5E5BAD5EEDu64)),
-                )
-            })
-        });
+                b.iter(|| {
+                    random_graph_er_concrete::<EdgeList, _>(
+                        i.0,
+                        i.1,
+                        black_box(SmallRng::seed_from_u64(0x0DDB1A5E5BAD5EEDu64)),
+                    )
+                })
+            });
     }
     group.finish();
 }
