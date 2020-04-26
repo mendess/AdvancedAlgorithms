692a08f96041efba8b679c31cc6e2c2d0a020f18
mendess <pedro.mendes.26@gmail.com>
2020-04-26 14:18:00 +0100

    Remove sorting

--- benches/graph_creation.rs
+++ benches/graph_creation.rs
@@ -61,10 +61,7 @@ pub fn make_graphs(c: &mut Criterion) {
         group.bench_function(BenchmarkId::new("csr", format!("{}_{}_{}", n, p, e)), |b| {
             b.iter_batched(
                 || gen_edges(n, p),
-                |(nn, mut edges)| {
-                    edges.sort_unstable();
-                    CSR::from_edges(nn, edges)
-                },
+                |(nn, mut edges)| CSR::from_edges(nn, edges),
                 BatchSize::SmallInput,
             )
         });
