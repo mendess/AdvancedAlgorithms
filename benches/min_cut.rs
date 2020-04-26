9f922b727dc4a40cf82738e744210575ed834b98
mendess <pedro.mendes.26@gmail.com>
2020-04-26 19:16:36 +0100

    Add count versions of karger stein

--- benches/min_cut.rs
+++ benches/min_cut.rs
@@ -17,7 +17,7 @@ fn gen_graph(n: usize, p: f64) -> EdgeList {
     EdgeList::from_edges(n, random_graph_er(n, p, black_box(make_rng())))
 }
 
-pub fn random_graph_er_bench(c: &mut Criterion) {
+pub fn bench0(c: &mut Criterion) {
     let mut group = c.benchmark_group("MinCut");
     let params = [1_usize, 5, 9]
         .iter()
@@ -56,9 +56,48 @@ pub fn random_graph_er_bench(c: &mut Criterion) {
     group.finish();
 }
 
+pub fn bench1(c: &mut Criterion) {
+    let mut group = c.benchmark_group("MinCutCount");
+    let params = [1_usize, 5, 9]
+        .iter()
+        .map(|i| i * 10)
+        .flat_map(|n| [0.4, 0.5, 0.7].iter().map(move |&p| (n, p)))
+        .map(|(n, p)| {
+            let dist = Binomial::new((n * (n - 1) / 2).try_into().unwrap(), p).unwrap();
+            let e = dist.sample(&mut make_rng());
+            (n, p, e)
+        })
+        .map(|(n, p, e)| (n, p, e))
+        .collect::<Vec<_>>();
+    for (n, p, e) in params {
+        group.throughput(Throughput::Elements(e as u64));
+        group.bench_function(
+            BenchmarkId::new("karger_stein", format!("{}_{}_{}", n, p, e)),
+            |b| {
+                b.iter_batched(
+                    || gen_graph(n, p),
+                    |mut graph| min_cut::count::karger_stein_count(&mut graph),
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+        group.bench_function(
+            BenchmarkId::new("fast_karger_stein", format!("{}_{}_{}", n, p, e)),
+            |b| {
+                b.iter_batched(
+                    || gen_graph(n, p),
+                    |mut graph| min_cut::count::fast_karger_stein_count(&mut graph),
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+    group.finish();
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = random_graph_er_bench
+    targets = bench0, bench1
 }
 criterion_main!(benches);
--- src/algorithms/min_cut.rs
+++ src/algorithms/min_cut.rs
@@ -131,6 +131,133 @@ where
         .unwrap()
 }
 
+pub mod count {
+    use crate::{
+        graphs::{EdgeListGraph, WEdge},
+        util::disjoint_set::*,
+    };
+    use rand::{
+        distributions::uniform::{UniformInt, UniformSampler},
+        thread_rng,
+    };
+
+    fn contract_count<N, E, D>(
+        edges: &mut [WEdge<N, E>],
+        ds: &mut D,
+        comp: usize,
+        cur_node: &mut usize,
+    ) -> usize
+    where
+        N: Clone,
+        E: Clone,
+        D: DisjointSet,
+    {
+        let mut rng = thread_rng();
+        let mut cur = *cur_node;
+        while cur < edges.len() && ds.components() > comp {
+            let i = UniformInt::<usize>::sample_single(cur, edges.len(), &mut rng);
+            edges.swap(cur, i);
+            if !ds.are_connected(edges[cur].0, edges[cur].1) {
+                ds.union(edges[cur].0, edges[cur].1);
+            }
+            cur += 1;
+        }
+        *cur_node = cur;
+        if comp == 2 {
+            edges.iter().filter(|e| !ds.are_connected(e.0, e.1)).count()
+        } else {
+            Default::default()
+        }
+    }
+
+    fn min_cut_count<N, E, D>(edges: &mut [WEdge<N, E>], mut ds: D, current_node: usize) -> usize
+    where
+        N: Clone,
+        E: Clone,
+        D: DisjointSet + Clone,
+    {
+        if ds.components() < 6 {
+            let mut cur = current_node;
+            contract_count(edges, &mut ds, 2, &mut cur)
+        } else {
+            let t = 1 + (ds.components() as f64 / 2.0_f64.sqrt()) as usize;
+            let mut updateable_node = current_node;
+            let mut ds1 = ds.clone();
+            contract_count(edges, &mut ds1, t, &mut updateable_node);
+            let m1 = min_cut_count(edges, ds1, updateable_node);
+            updateable_node = current_node;
+            let mut ds2 = ds.clone();
+            contract_count(edges, &mut ds2, t, &mut updateable_node);
+            let m2 = min_cut_count(edges, ds2, updateable_node);
+            if m1 < m2 {
+                m1
+            } else {
+                m2
+            }
+        }
+    }
+
+    pub fn karger_stein_count<G>(edges: &mut G) -> usize
+    where
+        G::NodeWeight: Clone,
+        G::EdgeWeight: Clone,
+        G: EdgeListGraph,
+    {
+        let n_vert = edges.vertices();
+        let edges = edges.as_edges_mut();
+        let log = (!n_vert).trailing_zeros();
+        let runs = log * log + 2;
+        (0..runs)
+            .map(|_| min_cut_count(edges, SimpleDisjointSet::new(n_vert), 0))
+            .min()
+            .unwrap()
+    }
+
+    fn fast_min_cut_count<N, E>(
+        edges: &mut [WEdge<N, E>],
+        ds: &mut UndoDisjointSet,
+        current_node: usize,
+    ) -> usize
+    where
+        N: Clone,
+        E: Clone,
+    {
+        if ds.components() < 6 {
+            let mut cur = current_node;
+            contract_count(edges, ds, 2, &mut cur)
+        } else {
+            let t = 1 + (ds.components() as f64 / 2.0_f64.sqrt()) as usize;
+            let mut updateable_node = current_node;
+            ds.save_state();
+            contract_count(edges, ds, t, &mut updateable_node);
+            let m1 = fast_min_cut_count(edges, ds, updateable_node);
+            ds.restore_state();
+            ds.save_state();
+            updateable_node = current_node;
+            contract_count(edges, ds, t, &mut updateable_node);
+            let m2 = fast_min_cut_count(edges, ds, updateable_node);
+            ds.restore_state();
+            m1.min(m2)
+        }
+    }
+
+    pub fn fast_karger_stein_count<G>(edges: &mut G) -> usize
+    where
+        G::NodeWeight: Clone,
+        G::EdgeWeight: Clone,
+        G: EdgeListGraph,
+    {
+        let n_vert = edges.vertices();
+        let edges = edges.as_edges_mut();
+        let log = (!n_vert).trailing_zeros();
+        let runs = log * log + 2;
+        (0..runs)
+            .map(|_| fast_min_cut_count(edges, &mut UndoDisjointSet::new(n_vert), 0))
+            .min()
+            .unwrap()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::graphs::{edge_list::EdgeList, test_graphs, FromEdges};
